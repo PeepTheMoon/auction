8476ac58a433b8758cb56a3a181218b23ab95338
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-15 16:47:07 -0700

    adds post route for auction with passing test

---
+++ __tests__/auctions.test.js
@@ -0,0 +1,56 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+
+describe('auction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('creates a new auction with POST', async() => {
+    const user = await User.create({
+      email: 'cheese@cheesy.com',
+      password: 'hotcheesesammy'
+    });
+
+    return request(app)
+      .post('/api/v1/auctions')
+      .auth('cheese@cheesy.com', 'hotcheesesammy')
+      .send({
+        user: user._id,
+        title: 'Guitar for sale',
+        description: 'Gibson electric guitar',
+        quantity: '1',
+        end: '2020-07-19'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          user: user.id,
+          title: 'Guitar for sale',
+          description: 'Gibson electric guitar',
+          quantity: '1',
+          end: '2020-07-19T00:00:00.000Z',
+          __v: 0
+        });
+      });
+  });
+
+
+});
