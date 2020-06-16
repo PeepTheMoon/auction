f29c2655e8b56aaf78f7fe8ebdb10b864a83b33b
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-15 17:24:24 -0700

    adds bid schema to auction model, sets up bid route and bid test files

---
+++ __tests__/bids.test.js
@@ -0,0 +1,29 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+const Bid = require('../lib/models/Bid');
+
+describe('auction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  
+
+});
